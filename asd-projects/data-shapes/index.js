b4f9d360ad3f5f453acd43c4f54d7c765d9ec1d4
DMcZeal59 <dwm0200357@lpssonline.com>
2024-03-05 15:59:59 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -45,17 +45,16 @@ $(document).ready(function () {
   };
   dataShapes.push(shape)
   // TODO 2: add a new property to all data shapes
-  let i = 0;
-  while (i < dataShapes.length) {
+  for (var i = 0; i < dataShapes.length; i++) {
     var currentShape = dataShapes[i]; 
 
     if (currentShape.color === "red"){
       currentShape.goodBehavior = "bounce";
     }
-    else if (color === "blue"){
+    else if (currentShape.color === "blue"){
       currentShape.goodBehavior = "blink";
     }
-    else{
+    else if (currentShape.color === "green"){
       currentShape.goodBehavior = "spin";
     }
   }
